9f0388d41d0ccfa0aa69daada5142b1442ffd016
rekonder <andrej.jugovic7@gmail.com>
2016-05-26 10:43:47 +0200

    Dodani endpointi za pacient dashboard

--- tpo10-rest/Controllers/ObservationController.cs
+++ tpo10-rest/Controllers/ObservationController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -98,8 +100,10 @@ namespace tpo10_rest.Controllers
                 }
             } 
         }
-       
+
         // GET: api/Observation/PatientProfile/{patientId}
+        [AllowAnonymous]
+        [HttpGet]
         [Route("PatientProfile/{patientId}")]
         [ResponseType(typeof(List<Observation>))]
         public async Task<IHttpActionResult> GetPatientObservations(Guid patientId)
@@ -116,6 +120,243 @@ namespace tpo10_rest.Controllers
             return Ok(observations.ToList());
         }
 
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetAlergies/{patientId}/{number}")]
+        [ResponseType(typeof(List<AlergiesViewModel>))]
+        public async Task<IHttpActionResult> GetAlergies(Guid patientId, int number)
+        {
+            var patient = db.PatientProfiles.Find(patientId) as PatientProfile;
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            var observations = db.Observations.Where(o => o.PatientProfile.Id == patient.Id).OrderByDescending(s => s.ObservationTime);
+
+            List<AlergiesViewModel> alergies = new List<AlergiesViewModel>();
+
+            foreach (var observation in observations)
+            {
+                var alergiesObs = observation.Allergies;
+                foreach(var alergyObs in alergiesObs)
+                {
+                    AlergiesViewModel al = new AlergiesViewModel();
+                    al.Name = alergyObs.AllergyName;
+                    if (alergies.Count < number && !alergies.Any(f => f.Name == al.Name))
+                        alergies.Add(al);
+                    else
+                        break;
+                }
+                if (alergies.Count >= number)
+                    break;
+            }
+            return Ok(alergies.ToList());
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetDiases/{patientId}/{number}")]
+        [ResponseType(typeof(List<DiseaseViewModel>))]
+        public async Task<IHttpActionResult> GetDiases(Guid patientId, int number)
+        {
+            var patient = db.PatientProfiles.Find(patientId) as PatientProfile;
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            var observations = db.Observations.Where(o => o.PatientProfile.Id == patient.Id).OrderByDescending(s => s.ObservationTime);
+
+            List<DiseaseViewModel> diases = new List<DiseaseViewModel>();
+
+            foreach (var observation in observations)
+            {
+                var diseasesObs = observation.Diseases;
+                foreach (var diseaseObs in diseasesObs)
+                {
+                    DiseaseViewModel al = new DiseaseViewModel();
+                    al.DiseaseName = diseaseObs.DiseaseName;
+                    al.ObservationTime = observation.ObservationTime;
+                    al.DoctorName = observation.DoctorProfile.FirstName + ' ' + observation.DoctorProfile.LastName;
+                    if (observation.DoctorProfile.DocOrDentist == 0)
+                        al.DoctorName += ", zdravnik";
+                    else
+                        al.DoctorName += ", zobozdravnik";
+                    if (diases.Count < number && !diases.Any(f => f.DiseaseName == al.DiseaseName && f.ObservationTime == al.ObservationTime ))
+                        diases.Add(al);
+                    else
+                        break;
+                }
+                if (diases.Count >= number)
+                    break;
+            }
+            return Ok(diases.ToList());
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetDiets/{patientId}/{number}")]
+        [ResponseType(typeof(List<DietViewModel>))]
+        public async Task<IHttpActionResult> GetDiets(Guid patientId, int number)
+        {
+            var patient = db.PatientProfiles.Find(patientId) as PatientProfile;
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            var observations = db.Observations.Where(o => o.PatientProfile.Id == patient.Id).OrderByDescending(s => s.ObservationTime);
+
+            List<DietViewModel> diets = new List<DietViewModel>();
+
+            foreach (var observation in observations)
+            {
+                var dietsObs = observation.Diets;
+                foreach (var dietObs in dietsObs)
+                {
+                    DietViewModel al = new DietViewModel();
+                    al.ObservationTime = observation.ObservationTime;
+                    al.DietName = dietObs.DietName;
+                    al.DietUrl = dietObs.DietInstructions.ToList();
+                    al.DoctorName = observation.DoctorProfile.FirstName + ' ' + observation.DoctorProfile.LastName;
+                    if (observation.DoctorProfile.DocOrDentist == 0)
+                        al.DoctorName += ", zdravnik";
+                    else
+                        al.DoctorName += ", zobozdravnik";
+                    if (diets.Count < number  && !diets.Any(f =>f.DietName == al.DietName && f.ObservationTime == al.ObservationTime))
+                        diets.Add(al);
+                    else
+                        break;
+                }
+                if (diets.Count >= number)
+                    break;
+            }
+            return Ok(diets.ToList());
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetMedications/{patientId}/{number}")]
+        [ResponseType(typeof(List<MedicationsViewModel>))]
+        public async Task<IHttpActionResult> GetMedications(Guid patientId, int number)
+        {
+            var patient = db.PatientProfiles.Find(patientId) as PatientProfile;
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            var observations = db.Observations.Where(o => o.PatientProfile.Id == patient.Id).OrderByDescending(s => s.ObservationTime);
+
+            List<MedicationsViewModel> medications = new List<MedicationsViewModel>();
+
+            foreach (var observation in observations)
+            {
+                var medicationsObs = observation.Medications;
+                foreach (var medicationObs in medicationsObs)
+                {
+                    MedicationsViewModel al = new MedicationsViewModel();
+                    al.ObservationTime = observation.ObservationTime;
+                    al.MedicationName = medicationObs.MedicationName;
+                    al.MedicationUrl = medicationObs.MedicationInstruction.Url;
+                    al.DoctorName = observation.DoctorProfile.FirstName + ' ' + observation.DoctorProfile.LastName;
+                    if (observation.DoctorProfile.DocOrDentist == 0)
+                        al.DoctorName += ", zdravnik";
+                    else
+                        al.DoctorName += ", zobozdravnik";
+                    if (medications.Count < number && !medications.Any(f => f.MedicationName == al.MedicationName && f.ObservationTime == al.ObservationTime))
+                        medications.Add(al);
+                    else
+                        break;
+                }
+                if (medications.Count >= number)
+                    break;
+            }
+            return Ok(medications.ToList());
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetMeasurements/{patientId}/{number}")]
+        [ResponseType(typeof(List<MeasurementsViewModel>))]
+        public async Task<IHttpActionResult> GetMeasurements(Guid patientId, int number)
+        {
+            var patient = db.PatientProfiles.Find(patientId) as PatientProfile;
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            var observations = db.Observations.Where(o => o.PatientProfile.Id == patient.Id).OrderByDescending(s => s.ObservationTime);
+
+            List<MeasurementsViewModel> measurements = new List<MeasurementsViewModel>();
+
+            foreach (var observation in observations)
+            {
+                var measurementsObs = observation.ObservationMeasurements;
+                foreach (var measurementObs in measurementsObs)
+                {
+                    MeasurementsViewModel al = new MeasurementsViewModel();
+                    al.MeasurementTime = measurementObs.MeasurementTime;
+                    al.Value = measurementObs.Value;
+                    if (measurementObs.MeasurementPart != null)
+                    {
+                        al.MeasurementPartName = measurementObs.MeasurementPart.MeasurementPartName;
+                        al.MeasurementUnit = measurementObs.MeasurementPart.MeasurementUnit;
+                    }
+                    else
+                    {
+                        al.MeasurementPartName = "Ni na voljo";
+                        al.MeasurementUnit = "e";
+                    }
+                    al.DoctorName = observation.DoctorProfile.FirstName + ' ' + observation.DoctorProfile.LastName;
+                    if (observation.DoctorProfile.DocOrDentist == 0)
+                        al.DoctorName += ", zdravnik";
+                    else
+                        al.DoctorName += ", zobozdravnik";
+                    if (measurements.Count < number && !measurements.Any(f => f.MeasurementPartName == al.MeasurementPartName && al.MeasurementPartName != "Ni na voljo" && f.MeasurementTime == al.MeasurementTime))
+                        measurements.Add(al);
+                    else
+                        break;
+                }
+                if (measurements.Count >= number)
+                    break;
+            }
+            return Ok(measurements.OrderByDescending(s =>s.MeasurementTime).ToList());
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("OldObservations/{patientId}/{number}")]
+        [ResponseType(typeof(List<OldObservationsViewModel>))]
+        public async Task<IHttpActionResult> OldObservations(Guid patientId, int number)
+        {
+            var patient = db.PatientProfiles.Find(patientId) as PatientProfile;
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            var observations = db.Observations.Where(o => o.PatientProfile.Id == patient.Id).OrderByDescending(s => s.ObservationTime);
+
+            List<OldObservationsViewModel> oldObservations = new List<OldObservationsViewModel>();
+
+            foreach (var observation in observations)
+            {
+                OldObservationsViewModel al = new OldObservationsViewModel();
+                al.ObservationTime = observation.ObservationTime;
+                al.DoctorName = observation.DoctorProfile.FirstName + ' ' + observation.DoctorProfile.LastName;
+                if (observation.DoctorProfile.DocOrDentist == 0)
+                    al.DoctorName += ", zdravnik";
+                else
+                    al.DoctorName += ", zobozdravnik";
+                if (oldObservations.Count < number)
+                    oldObservations.Add(al);
+                else
+                    break;
+            }
+            return Ok(oldObservations.ToList());
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -125,6 +366,15 @@ namespace tpo10_rest.Controllers
             }
             base.Dispose(disposing);
         }
-        
+        [AllowAnonymous]
+        [AcceptVerbs("OPTIONS")]
+        public HttpResponseMessage Options()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Headers", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            return response;
+        }
     }
 }
---
+++ tpo10-rest/Models/ObservationViewModels.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tpo10_rest.Models
+{
+    public class ObservationViewModels
+    {
+    }
+
+    public class AlergiesViewModel
+    {
+        public string Name { get; set; }
+    }
+    public class OldObservationsViewModel
+    {
+        public DateTime ObservationTime { get; set; }
+        public string DoctorName { get; set; }
+    }
+    public class DiseaseViewModel
+    {
+        public DateTime ObservationTime { get; set; }
+        public string DiseaseName { get; set; }
+        public string DoctorName { get; set; }
+    }
+    public class DietViewModel
+    {
+        public DateTime ObservationTime { get; set; }
+        public string DietName { get; set; }
+        public List<DietInstruction> DietUrl { get; set; }
+        public string DoctorName { get; set; }
+
+    }
+    public class MedicationsViewModel
+    {
+        public DateTime ObservationTime { get; set; }
+        public string MedicationName { get; set; }
+        public string MedicationUrl { get; set; }
+        public string DoctorName { get; set; }
+
+    }
+    public class MeasurementsViewModel
+    {
+        public DateTime MeasurementTime { get; set; }
+        public double Value { get; set; }
+        public string MeasurementPartName { get; set; }
+        public string MeasurementUnit { get; set; }
+        public string DoctorName { get; set; }
+
+    }
+}
